0b862b53aebf8f722df369553b9964d43f15a619
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-17 00:39:41 -0800

    partial refactoring

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,34 +1,9 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
-
-  const useRecord = (init) => {
-  const [before, setBefore] = useState([]);
-  const [current, setCurrent] = useState(init);
-  const [after, setAfter] = useState([]);
-
-  const undo = () => {
-    setAfter(after => [current, ...after]);
-    setCurrent(before[before.length - 1]);
-    setBefore(before => before.slice(0, -1));
-  };
-
-  const redo = () => {
-    setBefore(before => [...before, current]);
-    setCurrent(after[0]);
-    setAfter(after => after.slice(1));
-  };
-
-  const record = val => {
-    setBefore(before => [...before, current]);
-    setCurrent(val);
-  };
-
-  return {
-    undo,
-    record,
-    redo,
-    current,
-  };
+const initialState = {
+  color: 'green',
+  before: [],
+  after: []
 };
 
 function App() {
